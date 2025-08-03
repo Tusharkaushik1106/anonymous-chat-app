192c254b5049702d63d2f4af74d3aa18244b3a28
Tusharkaushik1106 <144094757+Tusharkaushik1106@users.noreply.github.com>
2025-08-03 22:12:01 +0530

    Initial UI design using shadcn

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,6 +1,62 @@
-import React from 'react'
+'use client'
+
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import * as z from "zod"
+import Link from "next/link"
+import { useState, useEffect } from "react" 
+import {useDebounceValue} from 'usehooks-ts'
+import { toast, useSonner } from "sonner"
+import { useRouter } from "next/navigation"
+import { signupSchema } from "@/schemas/signupSchema"
+import axios, { AxiosError } from 'axios'
+import { ApiResponse } from "@/types/ApiResponse"
+
 
 function page() {
+
+  const [username, setUsername] = useState('')
+  const [usernameMessage , setUsernameMessage] = useState('')
+  const [isCheckingUsername, setIsCheckingUsername] = useState(false)
+  const [isSubmitting, setIsSubmitting] = useState(false)
+
+  const debouncedUsername = useDebounceValue(username,300)
+  const {toasts} = useSonner()
+
+  const router = useRouter()
+
+  const form = useForm<z.infer<typeof signupSchema>>({
+    resolver: zodResolver(signupSchema),
+    defaultValues:{
+      username:'',
+      email:'',
+      password:''
+    }
+  })
+
+  useEffect(() => {
+    const checkUsernameUnique= async ()=>{
+      if(debouncedUsername){
+        setIsCheckingUsername(true)
+        setUsernameMessage("")
+        try {
+          const response = await axios.get( `/api/check-username-unique?username=${debouncedUsername}`)
+          setUsernameMessage(response.data.message)
+        } catch (error) {
+          const axiosError = error as AxiosError<ApiResponse>
+          setUsernameMessage(axiosError.response?.data.message ?? "error checking username")
+        } finally{
+          setIsCheckingUsername(false)
+        }
+      }
+    }
+    checkUsernameUnique()
+  },
+     [debouncedUsername])
+
+  
+  
+
   return (
     <div>page</div>
   )
